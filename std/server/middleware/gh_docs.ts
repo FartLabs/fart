da423d6d1c219c986322c0a4528577f6373e7c8c
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-10-13 21:17:37 -0700

    Update gh_docs.ts

--- std/server/middleware/gh_docs.ts
+++ std/server/middleware/gh_docs.ts
@@ -4,7 +4,9 @@ import {
   join,
   normalize,
 } from "../../../deps/std/path.ts";
+import { marked as parse } from "../../../deps/third_party/marked.ts";
 import { convertFilenameToTargetFilename } from "../../common.ts";
+import { Mime } from "../../common.ts";
 
 const fetchDoc = async (pathname: string): Promise<string | undefined> => {
   // TODO: Do not check if --allow-env is unspecified.
@@ -20,28 +22,46 @@ const fetchDoc = async (pathname: string): Promise<string | undefined> => {
     ),
   );
   try {
-    await Deno.readFile(docPath);
-    return docPath;
+    const decoder = new TextDecoder("utf-8");
+    const docFile = await Deno.readFile(docPath);
+    return decoder.decode(docFile);
     // deno-lint-ignore no-empty
   } catch {}
 };
 
-const processUrl = (pathname: string, hash?: string): string => {
-  const BASE_URL = "https://etok.codes/fart/blob/main/docs/";
-  const targetName = convertFilenameToTargetFilename(pathname, ".md");
-  hash = hash !== undefined && hash.length > 0 ? hash : "#readme";
-  return join(BASE_URL + targetName) + hash;
-};
+// const processUrl = (pathname: string, hash?: string): string => {
+//   const BASE_URL = "https://etok.codes/fart/blob/main/docs/";
+//   const targetName = convertFilenameToTargetFilename(pathname, ".md");
+//   hash = hash !== undefined && hash.length > 0 ? hash : "#readme";
+//   return join(BASE_URL + targetName) + hash;
+// };
 
 export default async (
   pathname: string,
   hash?: string,
 ): Promise<Response | undefined> => {
   try {
-    const docPath = await fetchDoc(pathname);
-    if (docPath !== undefined) {
-      const redirectUrl = processUrl(pathname, hash);
-      return Response.redirect(redirectUrl, 302);
+    const doc = await fetchDoc(pathname);
+    if (doc !== undefined) {
+      console.log(hash);
+      const html = parse(doc);
+      return new Response(
+        `<html>
+  <head>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <main id="wrapper">
+      <img src="/fart-logo.png" alt="Fart Logo" style="float: right; width: 144px;" />
+      ${html}
+    </main>
+  </body>
+</html>`,
+        {
+          status: 200,
+          headers: { "Content-Type": Mime.HTML },
+        },
+      );
     }
     // deno-lint-ignore no-empty
   } catch {}
