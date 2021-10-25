7d5f3a6d1d18fda83945f22aa038ffa6eb20338d
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-10-24 22:02:26 -0700

    moved cli to legacy directory

--- std/fart/ts/fart_department.ts
+++ std/fart/ts/fart_department.ts
@@ -1,4 +1,5 @@
 import type {
+  CompilerSettings,
   FartDepartment as fFartDepartment,
 } from "https://fart.tools/ts/EthanThatOneKid/fart/main/std/fart/fart.ts";
 import { IODepartment as fIODepartment } from "https://fart.deno.dev/ts/EthanThatOneKid/fart/main/std/io/io.ts";
@@ -9,9 +10,11 @@ import cartridges from "../../carts/mod.ts";
 export class FartDepartment implements fFartDepartment {
   constructor(private io: fIODepartment) {}
 
-  async compile(filepath: string, cartId: string): Promise<string> {
+  async compile(settings: CompilerSettings): Promise<string> {
+    // deno-lint-ignore camelcase
+    const { filepath, cartridge_id } = settings;
     const content = await this.io.readFile(filepath);
-    const item = cartridges.vendor(cartId);
+    const item = cartridges.vendor(cartridge_id);
     if (item === undefined) return "";
     const { cartridge, typemap } = item;
     const code = await compile(content, {
