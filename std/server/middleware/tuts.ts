c43f2333458b2cbc40d167610d87e2a2e3f89885
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-10-28 01:14:16 -0700

    Create tuts.ts

---
+++ std/server/middleware/tuts.ts
@@ -0,0 +1,2 @@
+// Todo: Create UI with live code editor that shows result of all available output formats with how they connect to each line in the source Fart.
+// include a side bar with a brief explanation of the example code.
