0ebac47cd1a1c1bed56ee930bd359b600a5e092a
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-11-24 17:45:42 -0800

    progress

--- lib/tokenize/utils.ts
+++ lib/tokenize/utils.ts
@@ -14,15 +14,17 @@ export const findInLexicon = (
 };
 
 export const checkIsIdentifier = (candidate: string): boolean =>
-  /^[a-zA-Z_$]\.*[a-zA-Z_$0-9]*$/g.test(candidate);
+  /^[a-zA-Z_$][a-zA-Z0-9\._$]*$/.test(candidate);
 
-export const checkIsTextLiteral = (candidate: string): boolean =>
-  /^\`(.*?)\`$/g.test(candidate) ||
-  /^\'(.*?)\'$/g.test(candidate) ||
-  /^\"(.*?)\"$/g.test(candidate);
+export const checkIsTextLiteral = (candidate: string): boolean => {
+  return (candidate.startsWith("`") && candidate.endsWith("`")) ||
+    (candidate.startsWith("'") && candidate.endsWith("'")) ||
+    (candidate.startsWith('"') && candidate.endsWith('"'));
+};
 
 export const checkIsInlineComment = (candidate: string): boolean =>
-  /^;(.*?)$/.test(candidate);
+  candidate.startsWith(";");
 
-export const checkIsMultilineComment = (candidate: string): boolean =>
-  /^\/\*(.*?)\*\/$/.test(candidate);
+export const checkIsMultilineComment = (candidate: string): boolean => {
+  return candidate.startsWith("/*") && candidate.endsWith("*/");
+};
