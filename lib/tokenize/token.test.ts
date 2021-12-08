0163e0760464239142980fc0134781b7a860496d
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-12-08 00:19:44 -0800

    Update token.test.ts

--- lib/tokenize/token.test.ts
+++ lib/tokenize/token.test.ts
@@ -1,12 +1,23 @@
-// TODO(@ethandavidson): test `is` method
-// TODO(@ethandavidson): test `getKindOf` method
-// TODO(@ethandavidson): test `toString` method
-// TODO(@ethandavidson): test `value` method
-
-import { assertEquals } from "../../deps/std/testing.ts";
+import { assert, assertEquals } from "../../deps/std/testing.ts";
 import { Token } from "./token.ts";
 import { Lexicon } from "./lexicon.ts";
 
 Deno.test("creates a token with an empty string without crashing", () => {
   assertEquals(new Token("").kind, Lexicon.Unknown);
+});
+
+Deno.test("tokens can be accurately classified", () => {
+  assert(new Token("").is(Lexicon.Unknown));
+});
+
+Deno.test("raw strings can be accurately classified as a kind of token", () => {
+  assertEquals(Token.getKindOf(""), Lexicon.Unknown);
+});
+
+Deno.test("inherits the value of a token from its raw value", () => {
+  assertEquals(new Token("type").value, "type");
+});
+
+Deno.test("tokens are stringified based on their computed value property", () => {
+  assertEquals(new Token("type").toString(), "type");
 });
