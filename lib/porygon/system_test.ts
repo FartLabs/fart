31c051843745df5deb5365f06abc28eee06ee0fb
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-10-20 14:25:08 -0700

    proud, epic gamer moment 😎

---
+++ lib/porygon/system_test.ts
@@ -0,0 +1,29 @@
+import type { Schema, System } from "./system.ts";
+
+const SCHEMA = {
+  deno_deploy: {
+    project_name: "string",
+  },
+  google_sheets: {
+    sheet_id: "string",
+  },
+} as const satisfies Schema;
+
+const system: System<typeof SCHEMA> = {
+  components: [
+    {
+      kind: "deno_deploy",
+      fields: {
+        project_name: "string",
+      },
+    },
+    {
+      kind: "google_sheets",
+      fields: {
+        sheet_id: "string",
+      },
+    },
+  ],
+};
+
+console.log(system);
