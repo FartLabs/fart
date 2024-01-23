8c85205a29afa5ee5afbf492649a2623c50254ac
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-23 13:18:28 -0800

    Create greet_ethan.ts

---
+++ example/next_minimal/greet_ethan.ts
@@ -0,0 +1,6 @@
+// TODO: Define a local file or data as a reusable message or service parsing intention.
+// A component is made up of components and input parameters.
+// Each component has a distinct role that it fits into for DRY and SOLID.
+
+// For example, a TypeScript Class component represents generating a class in a programming language.
+// I think I want to make a language-agnostic ts-morph library that can be used to generate code in any language.
