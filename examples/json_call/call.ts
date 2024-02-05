2e8b83b08912b11fbddcb5217ef61e9eb86a7904
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-02-05 11:01:11 -0800

    Update call.ts

--- examples/json_call/call.ts
+++ examples/json_call/call.ts
@@ -1,5 +1,73 @@
 import { Eta } from "./developer_deps.ts";
 
+// deno run --allow-read examples/json_call/call.ts > examples/json_call/call_generated.ts
+// deno run examples/json_call/call_generated.ts
+if (import.meta.main) {
+  // Note: type-safety is sound.
+  // function generateGreet(id = "greet", defaultName = "world") {
+  //   return `function ${id}(name = "${defaultName}") {
+  // return \`Hello, \${name}!\`;
+  // }`;
+  // }
+  // const simpleGreetCall0: CallOf<
+  //   "generateGreet",
+  //   typeof generateGreet
+  // > = { id: "generateGreet", args: ["greet", "world"] };
+  // const simpleGreetCall1: CallOf<
+  //   "generateGreet",
+  //   typeof generateGreet
+  // > = { id: "generateGreet", args: ["greet", 0] };
+
+  // Set up template engine.
+  const templateEngine = new Eta();
+  const executeGreetTemplate = templateEngine.compile(
+    // `import.meta.resolve("./greet.ts.tmpl")` doesn't work.
+    Deno.readTextFileSync("./examples/json_call/greet.ts.tmpl"),
+  );
+
+  // Define functions.
+  const fns = {
+    generateGreet(id = "greet", defaultName = "world") {
+      return templateEngine.render(
+        executeGreetTemplate,
+        { id, defaultName },
+      );
+    },
+    generateAdd(id = "add") {
+      return `function ${id}(a: number, b: number) {
+  return a + b;
+  }`;
+    },
+    generateMain(id = "main") {
+      return `function ${id}() {
+  console.log(greet(add(9, 10).toString()));
+  }
+  
+  if (import.meta.main) {
+    ${id}();
+  }`;
+    },
+  };
+
+  // TODO: fix type-safety.
+  const result1 = call(
+    fns,
+    { id: "generateGreet", args: ["greet", 0] },
+  );
+
+  const result2 = call(
+    fns,
+    { id: "generateAdd", args: ["add"] },
+  );
+
+  const result3 = call(
+    fns,
+    { id: "generateMain" },
+  );
+
+  console.log([result1, result2, result3].join("\n"));
+}
+
 /**
  * Call is JSON representation of a function call.
  */
@@ -15,15 +83,6 @@ export type Args = any[];
 export type Fn = (...args: Args) => any;
 
 /**
- * ArgsOf extracts the argument types of a function.
- */
-export type ArgsOf<TFn extends Fn> = TFn extends (
-  ...args: infer TArgs
-  // deno-lint-ignore no-explicit-any
-) => any ? TArgs
-  : never;
-
-/**
  * CallOf represents a call to a function with serialized arguments.
  */
 export interface CallOf<
@@ -31,7 +90,7 @@ export interface CallOf<
   TFn extends Fn,
 > {
   id: TID;
-  args: ArgsOf<TFn>;
+  args: Parameters<TFn>;
 }
 
 /**
@@ -55,57 +114,4 @@ function call<
   }
 
   return fn(...(c.args || []));
-}
-
-// deno run --allow-read examples/json_call/call.ts > examples/json_call/call_generated.ts
-// deno run examples/json_call/call_generated.ts
-if (import.meta.main) {
-  // Set up template engine.
-  const templateEngine = new Eta();
-  const executeGreetTemplate = templateEngine.compile(
-    // I wish `import.meta.resolve("./greet.ts.tmpl")` worked, but it doesn't.
-    Deno.readTextFileSync("./examples/json_call/greet.ts.tmpl"),
-  );
-
-  // Define functions.
-  const fns = {
-    generateGreet(id = "greet", defaultName = "world") {
-      return templateEngine.render(
-        executeGreetTemplate,
-        { id, defaultName },
-      );
-    },
-    generateAdd(id = "add") {
-      return `function ${id}(a: number, b: number) {
-return a + b;
-}`;
-    },
-    generateMain(id = "main") {
-      return `function ${id}() {
-console.log(greet(add(9, 10).toString()));
-}
-
-if (import.meta.main) {
-  ${id}();
-}`;
-    },
-  };
-
-  // TODO: fix type-safety.
-  const result1 = call(
-    fns,
-    { id: "generateGreet", args: ["greet", 0] },
-  );
-
-  const result2 = call(
-    fns,
-    { id: "generateAdd", args: ["add"] },
-  );
-
-  const result3 = call(
-    fns,
-    { id: "generateMain" },
-  );
-
-  console.log([result1, result2, result3].join("\n"));
 }
