8bd577b8b5b818a0f66c452dc1df8e56889a31d3
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-02-13 12:43:29 -0800

    wip

--- examples/extend_sk/main.ts
+++
@@ -1,5 +0,0 @@
-// File: examples/extend_sk/main.ts
-// Description: Extends an existing SvelteKit app with a new route.
-
-// https://github.com/EthanThatOneKid/sk_skeleton/tree/7963c4db951f29ff2c458783d7290eb09e680b3e
-// https://api.github.com/repos/ethanthatonekid/sk_skeleton/git/trees/7963c4db951f29ff2c458783d7290eb09e680b3e?recursive=1
